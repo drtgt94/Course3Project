8143af5c2b129e9efec7087194edd589f15fca79
drtgt94 <drtgt94@users.noreply.github.com>
2015-06-18 16:07:54 -0500

    Code for project

---
+++ run_analysis.R
@@ -0,0 +1,94 @@
+#Load the activity and features data into R data frames
+
+activityLabel <- read.table("./data/UCI HAR Dataset/activity_labels.txt",
+        col.names = c("LabelId","Activity"))
+
+features <- read.table("./data/UCI HAR Dataset/features.txt",
+        col.names = c("FeatureId","Feature"))
+
+#Load the Training data into R data frames and set the column names in the
+#Training data frame based on the data from the features data
+
+TrainSet <- read.table("./data/UCI HAR Dataset/train/x_train.txt")
+colnames(TrainSet) <- features[,2]
+
+TrainLabels <- read.table("./data/UCI HAR Dataset/train/y_train.txt",
+        col.names = c("LabelId"))
+
+TrainSubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt",
+        col.names = c("SubjectId"))
+
+#Merge the Training data into a single data frame
+
+TrainTemp <- cbind(TrainSubjects, TrainLabels, TrainSet)
+FullTrain <- merge(activityLabel, TrainTemp, 
+                   by.x="LabelId", 
+                   by.y="LabelId", 
+                   all=TRUE)
+
+#Load the Test data into R data frames and set the column names in the
+#Test data frame based on the data from the features data
+
+TestSet <- read.table("./data/UCI HAR Dataset/test/x_test.txt")
+colnames(TestSet) <- features[,2]
+
+TestLabels <- read.table("./data/UCI HAR Dataset/test/y_test.txt",
+        col.names = c("LabelId"))
+
+TestSubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt",
+        col.names = c("SubjectId"))
+
+#Merge the Test data into a single data frame
+
+TestTemp <- cbind(TestSubjects, TestLabels, TestSet)
+FullTest <- merge(activityLabel, TestTemp, 
+                   by.x="LabelId", 
+                   by.y="LabelId", 
+                   all=TRUE)
+
+#Merge the Training and Test data frames into a single data frame
+FullDataset <- merge(FullTest, FullTrain, all=TRUE)
+
+#Extract column names from the data frame
+
+FullDatasetColNames <- colnames(FullDataset)
+
+#Identify the columns that contain the Mean and Standard Deviations
+
+meanColumns <- grep("mean()",FullDatasetColNames, fixed=TRUE)
+stdColumns  <- grep("std()",FullDatasetColNames, fixed=TRUE)
+relevantColumns <- c(2, 3, meanColumns, stdColumns)
+
+#Load the dplyr package
+
+library(dplyr)
+
+#Select only the relevant columns out of the full data frame
+
+FinalDataset <- select(FullDataset, relevantColumns)
+
+#Adjust the column names to remove special characters
+
+BadColumnNames <- colnames(FinalDataset)
+NewColumnNames <- gsub("\\()","",BadColumnNames)
+NewColumnNames <- gsub("-","",NewColumnNames)
+
+#Set the column names in the Final Dataset
+
+colnames(FinalDataset) <- NewColumnNames
+
+#create a list with Activity and Subject for grouping
+
+grpColumns <- names(FinalDataset)[1:2]
+dots <- lapply(grpColumns, as.symbol)
+
+#Create the final summarized dataset grouped by Activity and Subject
+
+summaryDataset <-
+FinalDataset %>%
+        group_by_(.dots=dots, add=TRUE) %>%
+        summarise_each(funs(mean))
+
+#Write the summarized dataset out to a csv file
+
+write.table(summaryDataset, file = "./data/final.txt", sep=",", row.names=FALSE)
